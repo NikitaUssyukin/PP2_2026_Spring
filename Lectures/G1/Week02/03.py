1ef71e6ee3d56c088f12b378d53d4019fdebcb8b
NikitaUssyukin <nikus0108@gmail.com>
2026-01-26 08:04:24 +0500

    upd

---
+++ Lectures/G1/Week02/03.py
@@ -0,0 +1,8 @@
+a = 1
+print(a, type(a))
+
+a = 3.14
+print(a, type(a))
+
+a = "Dog"
+print(a, type(a))
---
+++ Lectures/G1/Week02/17.py
@@ -0,0 +1,5 @@
+# Strings
+# another way to use single or double quotes inside a string
+a = 'which is escaping a character using a backslash - \" or \''
+# escaping a character using a backslash is used with special characters
+print(a)
